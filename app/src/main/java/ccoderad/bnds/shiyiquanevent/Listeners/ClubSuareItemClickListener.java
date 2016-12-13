28e60e90ff608cc86cc4174da1ab32d28f3bd669
AD1024 <ccoderad@gmail.com>
2016-12-13 08:38:09 +0800

    Custom OnClick Listener for RecyclerView

---
+++ app/src/main/java/ccoderad/bnds/shiyiquanevent/Listeners/ClubSuareItemClickListener.java
@@ -0,0 +1,11 @@
+package ccoderad.bnds.shiyiquanevent.Listeners;
+
+import android.view.View;
+
+/**
+ * Created by CCoderAD on 2016/10/28.
+ */
+
+public interface ClubSuareItemClickListener {
+    public void onClubItemClick(View v, int position);
+}
