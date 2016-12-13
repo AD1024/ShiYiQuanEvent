b981b29916556108ef8ff31773a81db5b1727a35
AD1024 <ccoderad@gmail.com>
2016-12-13 08:33:47 +0800

    Add new class for ClubSquareActivity

--- app/src/main/java/ccoderad/bnds/shiyiquanevent/Beans/ClubModel.java
+++ app/src/main/java/ccoderad/bnds/shiyiquanevent/Beans/ClubModel.java
@@ -1,12 +1,35 @@
 package ccoderad.bnds.shiyiquanevent.Beans;
 
+import android.util.Log;
+
+import java.io.Serializable;
+
+import ccoderad.bnds.shiyiquanevent.Global.URLConstants;
+
 /**
  * Created by CCoderAD on 16/5/12.
  */
-public class ClubModel {
+public class ClubModel{
+    private static String HOME_URL = URLConstants.HOME_URL;
     public String club_name;
     public String status;
     public String sname;
     public String LargeAvatarURL;
     public String mediumAvatarURL;
+    public String smallAvatarURL;
+
+    /*
+    * Add Prefix of Club Avatar
+    * */
+    public void parseURL(){
+        if(!LargeAvatarURL.contains(HOME_URL)){
+            LargeAvatarURL = HOME_URL+LargeAvatarURL;
+        }
+        if(!mediumAvatarURL.contains(HOME_URL)){
+            mediumAvatarURL = HOME_URL + mediumAvatarURL;
+        }
+        if(!smallAvatarURL.contains(HOME_URL)){
+            smallAvatarURL = HOME_URL + smallAvatarURL;
+        }
+    }
 }
