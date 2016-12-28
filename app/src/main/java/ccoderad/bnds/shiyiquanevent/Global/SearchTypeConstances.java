27eb0ea9845c98fab5fce7b4bdb1880082ac3efb
AD1024 <ccoderad@gmail.com>
2016-12-28 14:54:23 +0800

    Move Some Values to Global constances

---
+++ app/src/main/java/ccoderad/bnds/shiyiquanevent/Global/SearchTypeConstances.java
@@ -0,0 +1,13 @@
+package ccoderad.bnds.shiyiquanevent.Global;
+
+/**
+ * Created by CCoderAD on 2016/12/28.
+ */
+
+public class SearchTypeConstances {
+    public static final int TITLE = 0;
+    public static final int LOCATION = 1;
+    public static final int DATE = 2;
+    public static final int CLUB = 3;
+    public static final int UNIVERSAL = 4;
+}
---
+++ app/src/main/java/ccoderad/bnds/shiyiquanevent/Global/URLConstances.java
@@ -0,0 +1,12 @@
+package ccoderad.bnds.shiyiquanevent.Global;
+
+/**
+ * Created by CCoderAD on 2016/10/27.
+ */
+
+public class URLConstances {
+    public static final String HOME_URL = "http://shiyiquan.net/";
+    public static final String USER_AGENT = "Andriod%20APP%207ac0ce241d0b78af164e58d7e712e053";
+    public static final String QUARE_URL = "square/club/";
+    public static final String EVENT_URL = "event/";
+}
