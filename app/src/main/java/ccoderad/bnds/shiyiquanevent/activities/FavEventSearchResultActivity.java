cdb5a06ec9a3ec42242de869f632bb40b75f7d34
AD1024 <ccoderad@gmail.com>
2017-01-31 23:03:20 +0800

    stantardize code

--- app/src/main/java/ccoderad/bnds/shiyiquanevent/activities/FavEventSearchResultActivity.java
+++ app/src/main/java/ccoderad/bnds/shiyiquanevent/activities/FavEventSearchResultActivity.java
@@ -3,14 +3,16 @@ package ccoderad.bnds.shiyiquanevent.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
 import ccoderad.bnds.shiyiquanevent.adapters.FavEventListAdapter;
 import ccoderad.bnds.shiyiquanevent.beans.EventBean;
 import ccoderad.bnds.shiyiquanevent.R;
-import ccoderad.bnds.shiyiquanevent.global.SearchTypeConstances;
+import ccoderad.bnds.shiyiquanevent.global.SearchTypeConstants;
 
 public class FavEventSearchResultActivity extends AppCompatActivity {
 
@@ -19,39 +21,44 @@ public class FavEventSearchResultActivity extends AppCompatActivity {
     private List<EventBean> mSearchData;
     private FavEventListAdapter mAdapter;
     private int ViewType;
+    private TextView mNoResultIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fav_event_search_result);
         mSearchResultList = (ListView) findViewById(R.id.fav_event_search_result_list);
+        mNoResultIndicator = (TextView) findViewById(R.id.fav_event_search_result_no_result_indicator);
         Intent intent = getIntent();
         mSearchData = (List<EventBean>) intent.getSerializableExtra("SearchData");
         mAdapter = new FavEventListAdapter(this, mSearchData);
         mSearchResultList.setAdapter(mAdapter);
         ViewType = intent.getIntExtra("ViewType", -1);
+        if(mSearchData.size() == 0){
+            mNoResultIndicator.setVisibility(View.VISIBLE);
+        }
         switch (ViewType) {
             case -1: {
                 setTitle("Error呀QAQ");
                 break;
             }
-            case SearchTypeConstances.TITLE: {
+            case SearchTypeConstants.TITLE: {
                 setTitle("标题搜索结果");
                 break;
             }
-            case SearchTypeConstances.LOCATION: {
+            case SearchTypeConstants.LOCATION: {
                 setTitle("地点搜索结果");
                 break;
             }
-            case SearchTypeConstances.DATE: {
+            case SearchTypeConstants.DATE: {
                 setTitle("日期搜索结果");
                 break;
             }
-            case SearchTypeConstances.CLUB: {
+            case SearchTypeConstants.CLUB: {
                 setTitle("发起社团搜索结果");
                 break;
             }
-            case SearchTypeConstances.UNIVERSAL: {
+            case SearchTypeConstants.UNIVERSAL: {
                 setTitle("综合查询结果");
                 break;
             }
