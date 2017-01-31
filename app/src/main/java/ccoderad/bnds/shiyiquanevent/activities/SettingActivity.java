4e4761e20f76c0dba1a7ffbbfb83055452560428
AD1024 <ccoderad@gmail.com>
2017-01-31 22:58:10 +0800

    Add download alert

--- app/src/main/java/ccoderad/bnds/shiyiquanevent/activities/SettingActivity.java
+++ app/src/main/java/ccoderad/bnds/shiyiquanevent/activities/SettingActivity.java
@@ -1,17 +1,18 @@
 package ccoderad.bnds.shiyiquanevent.activities;
 
 import android.app.AlertDialog;
+import android.app.DownloadManager;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.imagepipeline.core.ImagePipeline;
@@ -23,8 +24,10 @@ import com.nostra13.universalimageloader.utils.StorageUtils;
 import java.io.File;
 
 import ccoderad.bnds.shiyiquanevent.R;
-import ccoderad.bnds.shiyiquanevent.global.PreferencesConstances;
-import ccoderad.bnds.shiyiquanevent.global.URLConstances;
+import ccoderad.bnds.shiyiquanevent.global.PreferencesConstants;
+import ccoderad.bnds.shiyiquanevent.global.URLConstants;
+import ccoderad.bnds.shiyiquanevent.utils.CacheUtils;
+import ccoderad.bnds.shiyiquanevent.utils.DownloadUtil;
 import ccoderad.bnds.shiyiquanevent.utils.ImageTools;
 import ccoderad.bnds.shiyiquanevent.utils.PreferenceUtils;
 import ccoderad.bnds.shiyiquanevent.utils.ToastUtil;
@@ -39,11 +42,14 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
     private CardView ClearEvent;
     private CardView mEnableHighQAvatar;
     private CardView mShowUpdateInfo;
+    private CardView mAutoCheckUpdate;
+    private CardView mCheckUpdate;
 
     private Switch SwitchEnableHighQAvatar;
     private Switch SwitchSkipSplash;
-    private SharedPreferences Preferences;
+    private Switch SwitchAutoCheckUpdate;
 
+    private SharedPreferences Preferences;
     private SharedPreferences.Editor gEditor;
 
     @Override
@@ -55,15 +61,20 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
         ImageLoader.getInstance().init(new ImageLoaderConfiguration.Builder(this).diskCache(new UnlimitedDiskCache(StorageUtils.getOwnCacheDirectory(this, "ShiyiquanImgs/Cache"))).build());
 
         // Init Preference
-        Preferences = getSharedPreferences(PreferencesConstances.SETTING_PREF, MODE_PRIVATE);
+        Preferences = getSharedPreferences(PreferencesConstants.SETTING_PREF, MODE_PRIVATE);
         gEditor = Preferences.edit();
 
         //ViewById
         CardSkipSplash = (CardView) findViewById(R.id.setting_skip_splash);
         ClearEvent = (CardView) findViewById(R.id.setting_clear_event_cache);
         ClearPicCache = (CardView) findViewById(R.id.setting_clear_pic_cache);
+
         SwitchSkipSplash = (Switch) findViewById(R.id.setting_switch_skip_splash);
         mShowUpdateInfo = (CardView) findViewById(R.id.setting_show_update_info);
+
+        mCheckUpdate = (CardView) findViewById(R.id.setting_check_update);
+        mAutoCheckUpdate = (CardView) findViewById(R.id.setting_enable_auto_check_update);
+        SwitchAutoCheckUpdate = (Switch) findViewById(R.id.setting_auto_check_update_switch);
 
         mEnableHighQAvatar = (CardView) findViewById(R.id.setting_enable_high_quality_avatar);
         SwitchEnableHighQAvatar = (Switch) findViewById(R.id.setting_high_quality_avatar_switch);
@@ -73,9 +84,17 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
         CardSkipSplash.setOnClickListener(this);
         ClearEvent.setOnClickListener(this);
         mShowUpdateInfo.setOnClickListener(this);
-        SwitchSkipSplash.setChecked(Preferences.getBoolean(PreferencesConstances.SETTING_SKIP_SPLASH_SCREEN_TAG, false));
         mEnableHighQAvatar.setOnClickListener(this);
-        SwitchEnableHighQAvatar.setChecked(Preferences.getBoolean(PreferencesConstances.SETTING_HIGH_QUALITY_AVATAR_TAG, false));
+        mAutoCheckUpdate.setOnClickListener(this);
+        mCheckUpdate.setOnClickListener(this);
+        SwitchSkipSplash.setChecked(Preferences
+                .getBoolean(PreferencesConstants.SETTING_SKIP_SPLASH_SCREEN_TAG, false));
+        SwitchEnableHighQAvatar.setChecked(Preferences
+                .getBoolean(PreferencesConstants.SETTING_HIGH_QUALITY_AVATAR_TAG, false));
+        SwitchAutoCheckUpdate
+                .setChecked(Preferences.getBoolean(PreferencesConstants
+                        .SETTING_ENABLE_AUTO_UPDATE_TAG, false));
+
 
         //Switch Listener
         SwitchSkipSplash.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -87,19 +106,35 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
         SwitchEnableHighQAvatar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                gEditor.putBoolean(PreferencesConstances.SETTING_HIGH_QUALITY_AVATAR_TAG, isChecked);
+                gEditor.putBoolean(PreferencesConstants.SETTING_HIGH_QUALITY_AVATAR_TAG, isChecked);
                 gEditor.apply();
-                if(isChecked){
-                    ToastUtil.makeText("已切换高质量头像，重启应用生效",false);
-                }else{
-                    ToastUtil.makeText("已切换普通质量头像，重启应用生效",false);
+                if (isChecked) {
+                    ToastUtil.makeText("已切换高质量头像，重启应用生效", false);
+                } else {
+                    ToastUtil.makeText("已切换普通质量头像，重启应用生效", false);
                 }
+            }
+        });
+        SwitchAutoCheckUpdate.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setAutoUpdateCheckAvailability(isChecked);
             }
         });
     }
 
+    private void setAutoUpdateCheckAvailability(boolean check) {
+        if (check) {
+            ToastUtil.makeText("已开启自动检查更新", false);
+        } else {
+            ToastUtil.makeText("已关闭自动检查更新", false);
+        }
+        gEditor.putBoolean(PreferencesConstants.SETTING_ENABLE_AUTO_UPDATE_TAG, check);
+        gEditor.apply();
+    }
+
     private void setSplashAvailability(boolean check) {
-        gEditor.putBoolean(PreferencesConstances.SETTING_SKIP_SPLASH_SCREEN_TAG, check);
+        gEditor.putBoolean(PreferencesConstants.SETTING_SKIP_SPLASH_SCREEN_TAG, check);
         gEditor.apply();
     }
 
@@ -144,7 +179,7 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
         ImagePipeline pipe = Fresco.getImagePipeline();
         pipe.clearDiskCaches();
         pipe.clearMemoryCaches();
-        ToastUtil.makeText("已清理图片缓存",false);
+        ToastUtil.makeText("已清理图片缓存", false);
     }
 
     private void ClearEventCache() {
@@ -155,7 +190,70 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
                 del.delete();
             }
         }
-        ToastUtil.makeText("已删除活动缓存",false);
+        ToastUtil.makeText("已删除活动缓存", false);
+    }
+
+    private void showUpdateDownloadAlert() {
+        PreferenceUtils.shiftTable(this, PreferencesConstants.UPDATE_CHECKER_PREF, MODE_PRIVATE);
+
+        // Notification Header View
+        View updateNotiHeader = ViewTools
+                .Inflate(SettingActivity.this, R.layout.update_info_header, null);
+
+        TextView tvHeaderText = (TextView) updateNotiHeader
+                .findViewById(R.id.update_info_header_text);
+        tvHeaderText.setText("更新吗?");
+
+        // Alert Content View
+        View contentView = ViewTools
+                .Inflate(SettingActivity.this, R.layout.alert_update_download, null);
+        TextView tvCurrentVersion = (TextView) contentView
+                .findViewById(R.id.alert_update_download_current_version);
+        TextView tvNewVersion = (TextView) contentView
+                .findViewById(R.id.alert_update_download_new_version);
+        tvCurrentVersion.setText(URLConstants.CURRENT_VERSION);
+        tvNewVersion.setText(PreferenceUtils
+                .getString(PreferencesConstants.UPDATE_CHECKER_NEW_VERSION_CODE, "NAN"));
+
+        int[] color = ImageTools.RandomColor();
+        tvHeaderText.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
+        if (ImageTools.isDeepColor(color)) {
+            tvHeaderText.setTextColor(Color.WHITE);
+        }
+
+        final String fileUrl = PreferenceUtils.getString(PreferencesConstants.UPDATE_CHECKER_DOWNLOAD_LINK, "NAN");
+
+        if (fileUrl == null || fileUrl.equals("NAN")) {
+            ToastUtil.makeText("当前为最新版本", false);
+            return;
+        }
+
+        // Show Alert
+        new AlertDialog.Builder(SettingActivity.this)
+                .setCustomTitle(updateNotiHeader)
+                .setView(contentView)
+                .setPositiveButton("下载", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DownloadUtil.initialize(SettingActivity.this);
+                        DownloadManager.Request mRequest = new DownloadUtil.RequestBuilder(fileUrl)
+                                .setTitle("ShiYiQuanEvent" + URLConstants.CURRENT_VERSION + ".apk")
+                                .setDescription("正在下载新版十一圈")
+                                .setDownloadDirectory(Environment.DIRECTORY_DOWNLOADS
+                                        , "ShiYiQuanEvent-Update.apk")
+                                .setVisibilityInUi(true)
+                                .setMimeType("application/vnd.android.package-archive")
+                                .build();
+                        DownloadUtil.startDownload(mRequest);
+
+                    }
+                })
+                .setNegativeButton("暂不下载", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                }).show();
     }
 
     @Override
@@ -179,17 +277,37 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
                 ShowUpdateInfo();
                 break;
             }
-            case R.id.setting_enable_high_quality_avatar:{
+            case R.id.setting_enable_high_quality_avatar: {
                 boolean checked = SwitchEnableHighQAvatar.isChecked();
                 checked = !checked;
                 SwitchEnableHighQAvatar.setChecked(checked);
-                gEditor.putBoolean(PreferencesConstances
+                gEditor.putBoolean(PreferencesConstants
                         .SETTING_HIGH_QUALITY_AVATAR_TAG, checked);
                 gEditor.apply();
-                if(checked){
-                    ToastUtil.makeText("已切换高质量头像，重启应用生效",false);
-                }else{
-                    ToastUtil.makeText("已切换普通质量头像，重启应用生效",false);
+                if (checked) {
+                    ToastUtil.makeText("已切换高质量头像，重启应用生效", false);
+                } else {
+                    ToastUtil.makeText("已切换普通质量头像，重启应用生效", false);
+                }
+                break;
+            }
+            case R.id.setting_enable_auto_check_update: {
+                boolean isChecked = SwitchAutoCheckUpdate.isChecked();
+                isChecked = !isChecked;
+                SwitchAutoCheckUpdate.setChecked(isChecked);
+                break;
+            }
+            case R.id.setting_check_update: {
+                PreferenceUtils.initialize(this
+                        , PreferencesConstants.UPDATE_CHECKER_PREF
+                        , MODE_PRIVATE);
+                String newVersion = PreferenceUtils
+                        .getString(PreferencesConstants.UPDATE_CHECKER_NEW_VERSION_CODE
+                                , URLConstants.CURRENT_VERSION);
+                if (!URLConstants.CURRENT_VERSION.equals(newVersion)) {
+                    showUpdateDownloadAlert();
+                } else {
+                    ToastUtil.makeText("当前为最新版本", false);
                 }
                 break;
             }
---
+++ app/src/main/java/ccoderad/bnds/shiyiquanevent/utils/DownloadUtil.java
@@ -0,0 +1,168 @@
+package ccoderad.bnds.shiyiquanevent.utils;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ccoderad.bnds.shiyiquanevent.beans.DownloadRequestModel;
+
+/**
+ * Created by CCoderAD on 2017/1/6.
+ */
+
+public class DownloadUtil {
+    private static DownloadManager mManager;
+
+    private static List<Long> mDownloadQueue;
+
+    public static DownloadManager getInstance(Context context) {
+        if (mManager == null) {
+            mManager = (DownloadManager) context
+                    .getSystemService(Context.DOWNLOAD_SERVICE);
+        }
+        if (mDownloadQueue == null) {
+            mDownloadQueue = new ArrayList<>();
+        }
+        return mManager;
+    }
+
+    public static void initialize(Context context) {
+        mManager = (DownloadManager) context
+                .getSystemService(Context.DOWNLOAD_SERVICE);
+        mDownloadQueue = new ArrayList<>();
+    }
+
+    public static void startDownload(String url, String title, String description) {
+        if (mManager == null) {
+            Log.e("DownloadUtil", "Error: Uninitialized Util");
+            return;
+        }
+        DownloadManager.Request request = createRequest(url);
+        request.setDescription(description);
+        request.setTitle(title);
+        long id = mManager.enqueue(request);
+        mDownloadQueue.add(id);
+    }
+
+    public static void startDownload(DownloadManager.Request request) {
+        if (request == null) {
+            Log.e("DownloadUtil", "Error: Null Request");
+            return;
+        }
+        long id = mManager.enqueue(request);
+        mDownloadQueue.add(id);
+    }
+
+    public static boolean isInQueue(Long id) {
+        return mDownloadQueue.contains(id);
+    }
+
+    public static Uri getUriById(long id) {
+        return mManager.getUriForDownloadedFile(id);
+    }
+
+    public static void notifyFinished(Long id) {
+        mDownloadQueue.remove(id);
+    }
+
+    public static DownloadManager.Request createRequest(String url) {
+        return new DownloadManager.Request(Uri.parse(url));
+    }
+
+    public static DownloadManager.Request createRequest(String url, String title) {
+        DownloadManager.Request req = new DownloadManager.Request(Uri.parse(url));
+        req.setTitle(title);
+        return req;
+    }
+
+    public static DownloadManager.Request createRequest(String url, String title, String description) {
+        DownloadManager.Request req = new DownloadManager.Request(Uri.parse(url));
+        req.setTitle(title);
+        req.setDescription(description);
+        return req;
+    }
+
+    public static class RequestBuilder {
+        private DownloadRequestModel mRequest;
+
+        public RequestBuilder(String url) {
+            mRequest = new DownloadRequestModel();
+            mRequest.url = url;
+        }
+
+        public RequestBuilder setTitle(String title) {
+            mRequest.title = title;
+            return this;
+        }
+
+        public RequestBuilder setDescription(String description) {
+            mRequest.description = description;
+            return this;
+        }
+
+        public RequestBuilder setAllowedNetWorkType(int FLAG) {
+            mRequest.allowedNetworkFlag = FLAG;
+            return this;
+        }
+
+        public RequestBuilder setEnableOverMeter(boolean allowed) {
+            mRequest.allowedOverMeter = allowed;
+            return this;
+        }
+
+        public RequestBuilder setEnableRoaming(boolean allowed) {
+            mRequest.allowedOverRoaming = allowed;
+            return this;
+        }
+
+        public RequestBuilder setVisibilityInUi(boolean visible) {
+            mRequest.visibilityInUi = visible;
+            return this;
+        }
+
+        public RequestBuilder setDownloadDirectory(String parentPath, String subDir) {
+            mRequest.dirType = parentPath;
+            mRequest.subPath = subDir;
+            return this;
+        }
+
+        public RequestBuilder setMimeType(String mimeType){
+            mRequest.mimeType = mimeType;
+            return this;
+        }
+
+        public DownloadManager.Request build() {
+            DownloadManager.Request mRet = new DownloadManager.Request(Uri.parse(mRequest.url));
+            if (mRequest.title != null) {
+                mRet.setTitle(mRequest.title);
+            }
+            if (mRequest.description != null) {
+                mRet.setDescription(mRequest.description);
+            }
+            if (!TextUtils.isEmpty(mRequest.subPath) && !TextUtils.isEmpty(mRequest.dirType)) {
+                mRet.setDestinationInExternalPublicDir(mRequest.dirType, mRequest.subPath);
+            }
+            if (mRequest.allowedOverRoaming) {
+                mRet.setAllowedOverRoaming(true);
+            } else {
+                mRet.setAllowedOverRoaming(false);
+            }
+            if (mRequest.allowedOverMeter){
+                mRet.setAllowedOverMetered(true);
+            }else{
+                mRet.setAllowedOverMetered(false);
+            }
+            mRet.setAllowedNetworkTypes(mRequest.allowedNetworkFlag);
+            if(mRequest.mimeType != null && !TextUtils.isEmpty(mRequest.mimeType)){
+                mRet.setMimeType(mRequest.mimeType);
+            }
+            return mRet;
+        }
+    }
+}
